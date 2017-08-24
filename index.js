3b9795a9ac159fcf1682e8d845f2acf2b6fe098a
Ryan Tsao <rtsao@uber.com>
2017-08-24 13:02:02 -0700

    Update template

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = robot => {
   // Your plugin code here
   console.log('Yay, the plugin was loaded!');
-  
+
   // For more information on building plugins:
   // https://probot.github.io/docs/
-  
+
   // To get your plugin running against GitHub, see:
   // https://probot.github.io/docs/development/
 };
